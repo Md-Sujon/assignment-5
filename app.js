f7435928fecb234749d1c39bae9fe29ad90062fc
Md-Sujon <mdsujon829562@gmail.com>
2021-02-07 14:28:03 +0600

    1st

---
+++ app.js
@@ -0,0 +1,9 @@
+fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+.then(res=>res.json())
+.then(data=>displayMeal(data));
+
+const displayMeal=meal=>{
+    
+   
+    
+    }
