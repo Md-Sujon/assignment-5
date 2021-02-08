9979bfdb11299031baf16ea583650fa6adb8d021
Md-Sujon <mdsujon829562@gmail.com>
2021-02-08 19:09:59 +0600

    95%

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelectorAll('.meal-details-content')
-// const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-//even listeners
+
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click',getMealRecipe)
 
-//get meal that matches with the ingredients
+
 function getMealList(){
-  // console.log(searchInputTxt);
   let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
@@ -33,7 +31,7 @@ function getMealList(){
        });
         mealList.classList.remove('notFound')
        }else{
-         html="Sorry,Dur Meya valo kora nam dan!";
+         html="Sorry,একটা খাবারের নাম লেখতে পারেন না?? ভুল হয় কেমনে?ভালো করে লেখেন এসে পরবে!(JustFun)";
          mealList.classList.add('notFound')
        }
 
@@ -42,7 +40,6 @@ function getMealList(){
 }
 
 
-//recipe meals
 function getMealRecipe(a){
   a.preventDefault();
   if(a.target.classList.contains('recipe-btn')){
@@ -55,14 +52,14 @@ fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.i
 }
 
 function mealRecipeModal(meal){
-  console.log(meal);
-  meal=meal[0]
-  let html=`
-  <h2 class = "recipe-title">${meal.strMeal}</h2>
-  <p class = "recipe-category">${meal.strCategory}</p>
+
+const mealDiv=document.getElementById("meal-details-content");
+  mealDiv.innerHTML=`
+<h2>StrMeal:${meal.strMeal}</h2>
+  <p>StrCategory:${meal.strCategory}</p>
   <div class = "recipe-instruct">
     <h3>Instructions:</h3>
-    <p>${meal.strInstructions}</p>
+    <p>strInstructions:${meal.strInstructions}</p>
   </div>
   <div class = "recipe-meal-img">
     <img src = "${meal.strMealThumb}" alt = "food">
@@ -70,8 +67,6 @@ function mealRecipeModal(meal){
 
   `;
 }
-mealDetailsContent.innerHTML=html;
-mealDetailsContent.parentElement.classList.add('meal-details-content')
 
 
 
@@ -89,72 +84,4 @@ mealDetailsContent.parentElement.classList.add('meal-details-content')
 
 
 
-// const searchButton=document.getElementById("search-input").addEventListener("click",getMealList)
-// const mealList=document.getElementById("meal")
-// .addEventListener("click",getMealRecipe)
-// const mealDetailsContent=document.querySelector('meal-details-content');
 
-
-// function getMealList(){
-//   let  searchInputTxt=document.getElementById("search-input").value
-//   console.log(searchInputText);
-// fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
-// .then(response=>response.json())
-// .then(data => {
-//     let html ='';
-//     if(data.meals){
-//         data.meals.forEach(meal=>{
-//             html+=`
-//             <div class = "meal-item" data-id=${meal.idMeal}>
-//         <div class = "meal-img">
-//           <img src = "${meal.strMealThumb}" alt = "food">
-//         </div>
-//         <div class = "meal-name">
-//         <br>
-
-//        <h3>${meal.strMeal}</h3>
-          
-
-//         </div>
-//       </div>
-//             `;
-//         })
-//         // mealList.classList.remove('notFound')
-//     }else{
-//         html="sorry, no match";
-//         mealList.classList.add('notFound')
-//     }
-//     mealList.innerHTML=html;
-   
-// });
-// }
-
-// const mealDetails =strMeals=>{
-//     const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${strMeals}`
-// //    console.log(url);
-//     fetch(url)
-//     .then(res=>res.JSON())
-//     .then(data => renderMealInfo(data[0]));
-
-//     }
-
-
-    // meal recipe
-
-
-
-
-
-
-
-    // const renderMealInfo=meal=>{
-    //     // console.log(meal);
-    //     const  meals=document.getElementById("mealsDetails")
-    //     mealDetails.innerHTML=`
-    //     <h1>name:${meals.strMeal}</h1>
-    //     <p>strInstructions:${meals.strInstructions}</p>
-    //     <p>strArea:${meals.strArea}</p>
-    
-    //     `
-
-    // }
